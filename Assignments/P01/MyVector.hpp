d5e643ad09ed628a2c0373284831fa79b361f80d
Angel Badillo <badilloa022402@gmail.com>
2021-09-04 11:16:46 -0500

    commit on 'main'

---
+++ Assignments/P01/MyVector.hpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+// Node for our linked list
+struct node {
+    int data;  // data value (could be a lot more values)
+
+    node* next;  // we always need a "link" in a linked list
+
+    node(int x) {  // cunstructor to make adding values easy
+        data = x;
+        next = NULL;
+    }
+};
