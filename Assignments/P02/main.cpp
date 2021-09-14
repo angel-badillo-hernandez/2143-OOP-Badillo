9793446b7bc7241b496bead6d4d094a240f9f098
Angel Badillo <81447537+It-Is-Legend27@users.noreply.github.com>
2021-09-14 19:29:55 +0000

    update

---
+++ Assignments/P02/main.cpp
@@ -0,0 +1,29 @@
+/*****************************************************************************
+*                    
+*  Author:           Angel Badillo
+*  Email:            badilloa022402@gmail.com
+*  Label:            P01
+*  Title:            MyVector Class
+*  Course:           CMPS 2143
+*  Semester:         Fall 2021
+* 
+*  Description:
+*        This program implements a class called MyVector that is essentially
+*        a double linked list with overloaded constuctors that can append
+*        or prepend a double linked list to another double linked list.
+*        It can also add items to a specific location, search for an item
+*        and return the index, and remove an item at a specific location.
+*        
+*  Usage:
+*       - g++ main.cpp -o main && ./main
+*       - You just compile and run it.
+* 
+*  Files:     
+*       main.cpp        : driver program
+*       MyVector.hpp    : header file with definition and implementation
+*       input.dat       : input file
+*****************************************************************************/
+
+#include <iostream>
+#include <fstream>
+#
