bdeb962b852ba20ad610a1b4fa6e1e363469c28b
Angel Badillo <badilloa022402@gmail.com>
2021-10-01 22:27:11 -0500

    idk what im doing

--- Assignments/P03/Bolinverse.hpp
+++ Assignments/P03/Bolinverse.hpp
@@ -7,10 +7,12 @@ using namespace std;
 
 class BaseCharacter
 {
+protected:
     string name;
     string race;   // Does a character inherit from race? Or are they composed of a race?
     string gender; // Same for gender. Does race or gender offer any special properties that
                    // could be inherited by a character?
+    string classification;
     double health;
     double stamina;
     double skill;
@@ -19,15 +21,26 @@ class BaseCharacter
 
 public:
     // constructors
+    BaseCharacter() : name{""}, race{""}, gender{""}, health{100}, stamina{0}, skill{0} {}
 
+    // basic stuff
+    void takeDMG(double attack)
+    {
+        health -= attack;
+    }
+    
+    double HP()
+    {
+        return health;
+    }
     // pure virtual methods
-    virtual double attack() = 0;
+    virtual double attack(BaseCharacter*) = 0;
     virtual double defend() = 0;
 };
 
-
 class Weapon
 {
+private:
     string name;
     double skillLevel;
     double powerLevel;
@@ -44,10 +57,85 @@ struct Character
 
 class CharacterGen
 {
-    vector<string> races;
-    vector<string> classification;
-    string randRace();                    // randomly returns a race
-    string randClass();                   // randomly returns a classification
-    string weapon(string classification); // randomly assign a weapon based on character type
+protected:
+    string randRace()                    // randomly returns a race
+    {
+        if(rand()%100 > 100)
+        {
+            return "Human";
+        }
+        else
+        {
+            return "Elf";
+        }
+    }
+    string randClass()                 // randomly returns a classification
+    {
+        if(rand()%100 > 100)
+        {
+            return "Wizard";
+        }
+        else
+        {
+            return "Archer";
+        }
+    }
+    string randWeapon(string classification); // randomly assign a weapon based on character type
                                           // it could be a spell if the character is a wizard
+
+    string randGender()
+    {
+        return "male";
+    }
+};
+
+class Wizard : public BaseCharacter
+{
+private:
+    double mana;
+
+    double castSpell()
+    {
+        return rand() % 50;
+    }
+
+public:
+    double attack(BaseCharacter*)
+    {
+        double rtn = 0.0;
+        rtn = castSpell();
+
+        return rtn;
+    }
+
+    double defend()
+    {
+        double rtn = 0.0;
+
+        return rtn;
+    }
+};
+
+class Archer : public BaseCharacter
+{
+private:
+    double shootArrow()
+    {
+        return rand() % 50;
+    }
+public:
+    double attack(BaseCharacter* enemy)
+    {
+        double rtn = 0.0;
+        rtn = shootArrow();
+
+        enemy->takeDMG(rtn);
+
+        return rtn;
+    }
+    double defend()
+    {
+        double rtn = 0.0;
+        return rtn;
+    }
 };
--- Assignments/P03/main.cpp
+++ Assignments/P03/main.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <queue>
+#include "Bolinverse.hpp"
+
+using namespace std;
+
+int main()
+{
+    BaseCharacter* jojo = new Archer;
+    BaseCharacter* dio = new Wizard;
+
+    cout << "\nO\n";
+
+    jojo->attack(dio);
+
+    cout << "*oof*\n";
+
+    cout << "Dio has " << dio->HP() << " hp.\n";
+}
